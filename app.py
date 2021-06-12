238991ffb0e12569d7fefa0909d0d3ff5b0cfe8c
Emmanuella Tetteh <42817401+shikatetteh@users.noreply.github.com>
2021-06-12 17:55:09 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+name = input("Hello there, what's your name? "+ "")
+
+print("Hi" + " " + name);
+input("What's your favourite colour? "+ " ")
+colour=input()
+
+print(name + "'s" + " " + "favourite colour is"+" "+ colour)
